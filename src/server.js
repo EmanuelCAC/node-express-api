39908209e0f1a958d538f6ee3e657d117c5fb184
EmanuelCAC <emanuelc279@gmail.com>
2024-02-15 19:40:29 -0300

    project start

---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express"
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.json({message: "Hello World!"})
+})
+
+app.get('/produto', (req, res) => {
+    res.json({message: "Rota de produto"})
+  })
+
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta http://localhost:${port}`)
+})

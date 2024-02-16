f53914d825ff51c5ebe349f7a34acfc97ef26de2
EmanuelCAC <emanuelc279@gmail.com>
2024-02-15 22:14:46 -0300

    implementação do get, post, put e delete

---
+++ src/db-memory/user.js
@@ -0,0 +1,20 @@
+export const users = [
+    {
+        id: 1,
+        name: "Emanuel Cardoso",
+        email: "emanuelc279@gmail.com",
+        avatar: "https://avatars.githubusercontent.com/u/107555186?v=4"
+    },
+    {
+        id: 2,
+        name: "Júlia Noveline",
+        email: "9line@gmail.com",
+        avatar: "https://avatars.githubusercontent.com/u/106248989?v=4"
+    },
+    {
+        id: 3,
+        name: "Emilly Vieira",
+        email: "emilly@gmail.com",
+        avatar: "https://avatars.githubusercontent.com/u/90214744?v=4"
+    }
+]
--- src/server.js
+++ src/server.js
@@ -1,15 +1,59 @@
 import express from "express"
+import { PORT, HOST } from "./config.js"
+import { users } from "./db-memory/user.js"
+
 const app = express()
-const port = 3000
+
+app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.json({message: "Hello World!"})
+  res.json({ message: "Welcome to the API" })
 })
 
-app.get('/produto', (req, res) => {
-    res.json({message: "Rota de produto"})
+app.get('/user', (req, res) => {
+  res.json({
+    success: "Usúarios listados com sucesso!",
+    users
   })
+})
 
-app.listen(port, () => {
-  console.log(`Servidor rodando na porta http://localhost:${port}`)
+app.post('/user', (req, res) => {
+  const user = req.body
+  user.id = users[users.length - 1].id + 1;
+  users.push(user)
+
+  res.json({
+    success: "Usúarios listados com sucesso!",
+    users
+  })
+})
+
+app.put('/user', (req, res) => {
+  const user = req.body
+  for (let i = 0; i < users.length; i++) {
+    if (users[i].id == user.id) {
+      users[i].name = user.name || users[i].name
+      users[i].email = user.email || users[i].email
+      users[i].avatar = user.avatar || users[i].avatar
+    }
+  }
+
+  res.json({
+    success: "Usúarios listados com sucesso!",
+    users
+  })
+})
+
+app.delete('/user', (req, res) => {
+  const user = req.body
+  const newUser = users.filter((users) => users.id != user.id)
+
+  res.json({
+    success: "Usúarios listados com sucesso!",
+    newUser
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em ${HOST}:${PORT}`)
 })
